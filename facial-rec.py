8252b01759c18ad7badee26d6e571e54cc5cb59d
whopriyam <priyambasu16@gmail.com>
2020-03-19 18:39:42 +0530

    Add existing file

---
+++ facial-rec.py
@@ -0,0 +1,52 @@
+import facial_recognition
+import os
+import cv2
+
+known_faces_dir = 'me'
+unknown_faces_dir = 'group'
+tolerance = 0.6
+frame_thickness = 3
+font_thicknes = 2
+model = "cnn"
+
+print("We're loading known faces")
+
+known_faces = []
+known_names = []
+
+for name in os.listdir(known_faces_dir):
+	 for filename in os.listdir(f'{known_faces_dir}/{name}'):
+		image = face_recognition.load_image_file(f'{known_faces_dir}/{name}/{filename}')
+		encoding = face_recognition.face_encodings(image)[0]
+		known_faces.append(encoding)
+		known_names.append(name)
+
+print("Processing the unknown faces")
+
+for filename in os.listdir(unknown_faces_dir):
+
+    # Load image
+    print(filename)
+    image = face_recognition.load_image_file(f'{unknown_faces_dir}/{filename}')
+    locations = face_recognition.face_locations(image, model=model)
+    encodings = face_recognition.face_encodings(image, locations)
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+    for face_encoding, face_location in zip(encodings,locations):
+    	results = face_recognition.compare_faces(known_faces, face_encoding, tolerance)
+    	match = None
+    	if True in results:
+    		match = known_names[results.index(True)]
+    		print(f"Match found: {match}")
+    		top_left = (face_location[3],face_location[0])
+    		bottom_right = (face_location[1],face_location[2])
+    		color = [0,255,0]
+    		cv2.rectangle(image,top_left,bottom_right,color,frame_thickness)
+
+    		top_left = (face_location[3],face_location[2])
+    		bottom_right = (face_location[1],face_location[2]+22)
+    		cv2.rectangle(image,top_left,bottom_right,color,frame_thickness,cv2.FILLED)
+    		cv2.putText(image, match,(face_location[3]+10,face_location[2]+15),cv2.FONT_HERSHEY_SIMPLEX,0.5,(200,200,200),FONT_THICKNESS)
+    	cv2.imshow(filename_image)
+    	cv2.waitKey(5000)
+    	#cv2.destroyWindow(filename)
